7f1187a728c266810b6fdfd7ed8437f1c047f25b
lebeerman <omnidbeerman@gmail.com>
2018-03-21 14:53:11 -0600

    initial commit with gatsby template

---
+++ test-client/gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Starter Product Guy`,
+    author: `Product Guy`,
+    description: `👋 Open Source Developer`
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/content/projects`
+      }
+    },
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`
+      }
+    },
+    `gatsby-plugin-react-helmet`
+  ]
+};
